8b35e1159b65391c185ad5304975a287329b2747
Erick <erickbenitez336@gmail.com>
2021-09-06 16:31:44 -0400

    add signout button on home page after login/authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import "../styles/globals.css"
+
+import { Amplify } from "aws-amplify"
+import { AmplifyAuthenticator } from "@aws-amplify/ui-react"
+import awsExports from "../aws-exports"
+Amplify.configure({ ...awsExports, ssr: true })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AmplifyAuthenticator>
+      <Component {...pageProps} />
+    </AmplifyAuthenticator>
+  )
 }
 
 export default MyApp
