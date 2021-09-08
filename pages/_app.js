42247d4177c14c65beea9e4c95f9065b536bec51
tyjedz <65200721+tyjedz@users.noreply.github.com>
2021-09-08 01:09:49 -0400

    create basic file upload functionality to frontend

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,14 @@ import "../styles/globals.css"
 
 import { Amplify } from "aws-amplify"
 import { AmplifyAuthenticator } from "@aws-amplify/ui-react"
-import awsExports from "../aws-exports"
-Amplify.configure({ ...awsExports, ssr: true })
+// import awsExports from "../aws-exports"
+// Amplify.configure({ ...awsExports, ssr: true })
 
 function MyApp({ Component, pageProps }) {
   return (
-    <AmplifyAuthenticator>
+    //<AmplifyAuthenticator>
       <Component {...pageProps} />
-    </AmplifyAuthenticator>
+    //</AmplifyAuthenticator>
   )
 }
 
