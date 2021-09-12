2287a0e58b57ad120bd2a6e329d411663bec2193
aking98 <aking1@gatech.edu>
2021-09-12 12:55:44 -0400

    Fixed spacing on Home page

--- pages/components/DataUpload.jsx
+++ pages/components/DataUpload.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import styles from '../../styles/Home.module.css'
+
 import FileInput from './FileInput'
 import URLInput from './URLInput'
 import TextInput from './TextInput'
@@ -6,7 +8,7 @@ import TextInput from './TextInput'
 
 function DataUpload() {
   return (
-    <div className="dataUpload">
+    <div className={styles.dataUpload}>
       <div className="container">
         <div className="row align-items-center my-5">
             <div className="col-lg-10">
@@ -19,11 +21,17 @@ function DataUpload() {
                 </p>
             </div>
             <div>
-                <h2>- Option 1 - Upload CSV or Text File</h2>
+                <h2 className = {styles.uploadTitle}>
+                  - Option 1 - Upload CSV or Text File
+                </h2>
                     <FileInput/>
-                <h2>- Option 2 - Upload a Reddit Page&apos;s URL</h2>
+                <h2 className = {styles.uploadTitle}>
+                  - Option 2 - Upload a Reddit Page&apos;s URL
+                </h2>
                     <URLInput/>
-                <h2>- Option 3 - Upload Plain Text</h2>
+                <h2 className = {styles.uploadTitle}>
+                  - Option 3 - Upload Plain Text
+                </h2>
                     <TextInput/>
             </div>
 
